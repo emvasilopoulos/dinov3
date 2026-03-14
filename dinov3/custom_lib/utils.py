d51abc6e2a3f8479f0bb6cada8750aa83433a295
Manos <emmanouilvasilopoulos@gmail.com>
2026-03-15 00:35:42 +0200

    dinov3.custom_lib.utils: add convenxt_base and support optional pixel Normalization

--- dinov3/custom_lib/utils.py
+++ dinov3/custom_lib/utils.py
@@ -23,12 +23,29 @@ def load_convnext_small_pretrained_pytorch() -> torch.nn.Module:
     return model
 
 
+def load_convnext_base_pretrained_pytorch() -> torch.nn.Module:
+    base_convnext = dinov3.models.convnext.get_convnext_arch("convnext_base")
+    model = base_convnext(
+        patch_size=16,
+        drop_path_rate=0.0,
+    )
+
+    weights_path = TORCH_WEIGHTS_DIR / "dinov3_convnext_base_pretrain_lvd1689m-801f2ba9.pth"
+    cpkt = torch.load(weights_path.as_posix())
+    model.load_state_dict(cpkt, strict=True)
+    return model
+
+
 IMAGE_TRANSFORM: T.Compose = T.Compose([
     T.ToTensor(),
     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 
-def load_image_for_pretrained_model(image_path: pathlib.Path) -> Tensor:
+def load_image_for_pretrained_model(image_path: pathlib.Path,
+                                    normalize: bool = True) -> Tensor:
     image = PIL.Image.open(image_path).convert("RGB")
-    return IMAGE_TRANSFORM(image)
+    if normalize:
+        return IMAGE_TRANSFORM(image)
+    else:
+        return T.ToTensor()(image)
