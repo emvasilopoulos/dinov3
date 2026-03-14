4a16a842be5eb50a113899e74b3e80cb6a79e8a8
Manos <emmanouilvasilopoulos@gmail.com>
2026-03-15 00:36:30 +0200

    dinov3.sutom_lib.export: add resize staticmethods for easy experiments

--- dinov3/custom_lib/export/models.py
+++ dinov3/custom_lib/export/models.py
@@ -17,6 +17,42 @@ class ConvNextWithPreProcess(nn.Module):
             "std",
             torch.tensor(std, dtype=torch.float32).view(1, 3, 1, 1))
 
+    @staticmethod
+    def resize_image_vertical_small(x: torch.Tensor) -> torch.Tensor:
+        return nn.functional.interpolate(x,
+                                         size=(512, 384),
+                                         mode="bilinear",
+                                         align_corners=False)
+
+    @staticmethod
+    def resize_image_square_small(x: torch.Tensor) -> torch.Tensor:
+        return nn.functional.interpolate(x,
+                                         size=(384, 384),
+                                         mode="bilinear",
+                                         align_corners=False)
+
+    @staticmethod
+    def resize_image_vertical_large(x: torch.Tensor) -> torch.Tensor:
+        return nn.functional.interpolate(x,
+                                         size=(768, 512),
+                                         mode="bilinear",
+                                         align_corners=False)
+
+    @staticmethod
+    def resize_image_square_large(x: torch.Tensor) -> torch.Tensor:
+        return nn.functional.interpolate(x,
+                                         size=(512, 512),
+                                         mode="bilinear",
+                                         align_corners=False)
+
+    @staticmethod
+    def resize_image_vertical_large2(x: torch.Tensor) -> torch.Tensor:
+        # Bad results - keeping for reference
+        return nn.functional.interpolate(x,
+                                         size=(768, 384),
+                                         mode="bilinear",
+                                         align_corners=False)
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = (x - self.mean) / self.std
         return self.model(x)
