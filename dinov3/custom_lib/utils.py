3938c3ade4287c0752e669c1a9b73851ea0fc4c4
Manos <emmanouilvasilopoulos@gmail.com>
2026-03-15 13:26:16 +0200

    dinov3.custom_lib.utils: add save tensor as image function

--- dinov3/custom_lib/utils.py
+++ dinov3/custom_lib/utils.py
@@ -64,3 +64,27 @@ def load_image_for_pretrained_model(image_path: pathlib.Path,
         return IMAGE_TRANSFORM(image)
     else:
         return T.ToTensor()(image)
+
+
+def save_image_tensor(image_tensor: Tensor,
+                      save_path: pathlib.Path,
+                      normalize: bool = True) -> None:
+    # image_tensor shape (1, 3, H, W) or (3, H, W)
+    if image_tensor.dim() == 4:
+        image_tensor = image_tensor.squeeze(0)
+    if normalize:
+        # unnormalize
+        mean = torch.tensor([0.485, 0.456, 0.406],
+                            dtype=torch.float32).view(3, 1, 1)
+        std = torch.tensor([0.229, 0.224, 0.225],
+                           dtype=torch.float32).view(3, 1, 1)
+        unnormalized_image_tensor = image_tensor * std + mean
+    else:
+        unnormalized_image_tensor = image_tensor
+    # clamp to [0, 1]
+    unnormalized_image_tensor = torch.clamp(unnormalized_image_tensor, 0.0,
+                                            1.0)
+    # convert to PIL Image and save
+    unnormalized_image = T.ToPILImage()(unnormalized_image_tensor)
+    save_path.parent.mkdir(parents=True, exist_ok=True)
+    unnormalized_image.save(save_path.as_posix())
